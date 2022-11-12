cf41513ee526e63b92c73d3003833003053a0ec2
Antonio Mora <amora@digital.gob.cl>
2022-11-12 18:23:06 -0300

    Change template emails sents

--- resources/views/emails/contact/contact_mail.blade.php
+++ resources/views/emails/contact/contact_mail.blade.php
@@ -1,23 +1,29 @@
 <table>
     <thead style='text-align: center;'>
+        <tr><td><br></td></tr>
         <tr>
             <td style='border:none;' colspan='2'>
 	            <a href='https://biogaleno.com'>
-                    <img src="{{ asset('img/logo_biogaleno_2.png') }}" alt="" height="40">
+                    <img src="{{ asset('img/logo_biogaleno_2.png') }}" alt="" height="60">
                 </a>
                 <br><br>
 	        </td>
         </tr>
     </thead>
     <tbody>
+	    <tr><td><br></td></tr>
         <tr>
 	        <td style='border:none;'><strong>Nombre:</strong> {{ $data['name'] }}</td>
+	    </tr>
+	    <tr><td><br></td></tr>
+        <tr>
 	        <td style='border:none;'><strong>Email:</strong> {{ $data['email'] }}</td>
 	    </tr>
+	    <tr><td><br></td></tr>
 	    <tr>
             <td style='border:none;'><strong>Título:</strong> {{ $data['subject'] }}</td>
         </tr>
-	    <tr><td></td></tr>
+	    <tr><td><br></td></tr>
 	    <tr>
             <td colspan='2' style='border:none;'>
                 {{ $data['message'] }}
